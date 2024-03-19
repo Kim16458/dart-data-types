86ddb6688d6d6117396fe3149474e7b405bb0d84
Kim16458 <kirimiochola@gmail.com>
2024-03-19 17:34:43 +0300

    Create datatypes.dart

---
+++ datatypes.dart
@@ -0,0 +1,17 @@
+void main() {
+  int age = 15; /*int represents numbers*/
+  double time = 1.30; /*double is for numbers with decimal point*/
+  String city = "Nairobi"; /*String is for strings*/
+  List<int> numbers = [1, 2, 3, 4, 5]; /*list represents an ordered collection*/
+  Map<String, int> shopping = {
+    'Goats': 30,
+    'Sheep': 20,
+    'Cows': 10,
+  }; /*Maps are for key-valued pairs*/
+
+  print("Age: $age");
+  print("Time: $time");
+  print("City: $city");
+  print("Numbers: $numbers");
+  print("Shopping: $shopping");
+}
